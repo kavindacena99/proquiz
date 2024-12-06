0b582e2ac72c39c13a142f3e31aaead5578539c3
Kavinda Supun Dissanayake <102888367+kavindacena99@users.noreply.github.com>
2024-12-06 19:06:35 +0000

    database created in codespace

--- connection/connection.php
+++ connection/connection.php
@@ -1,15 +1,16 @@
 <?php
-    /*
     $hostname = "127.0.0.1";
     $username = "root";
     $password = "mariadb";
     $dbname = "proquiz";
-    */
+    
 
+    /*
     $hostname = "localhost";
     $username = "root";
     $password = "";
     $dbname = "proquiz";
+    */
 
     $connection = mysqli_connect($hostname,$username,$password,$dbname);
 ?>
